e9043b272319dab1a3263489249e0d69dde6a50d
yhy <31311038+yhy0@users.noreply.github.com>
2024-03-26 22:24:18 +0800

    update

--- sizedwaitgroup.go
+++ sizedwaitgroup.go
@@ -10,35 +10,35 @@
 package sizedwaitgroup
 
 import (
-	"context"
-	"math"
-	"sync"
+    "context"
+    "math"
+    "sync"
 )
 
 // SizedWaitGroup has the same role and close to the
 // same API as the Golang sync.WaitGroup but adds a limit of
 // the amount of goroutines started concurrently.
 type SizedWaitGroup struct {
-	Size int
-
-	current chan struct{}
-	wg      sync.WaitGroup
+    Size int
+    
+    current chan struct{}
+    wg      *sync.WaitGroup
 }
 
 // New creates a SizedWaitGroup.
 // The limit parameter is the maximum amount of
 // goroutines which can be started concurrently.
-func New(limit int) SizedWaitGroup {
-	size := math.MaxInt32 // 2^31 - 1
-	if limit > 0 {
-		size = limit
-	}
-	return SizedWaitGroup{
-		Size: size,
-
-		current: make(chan struct{}, size),
-		wg:      sync.WaitGroup{},
-	}
+func New(limit int) *SizedWaitGroup {
+    size := math.MaxInt32 // 2^31 - 1
+    if limit > 0 {
+        size = limit
+    }
+    return &SizedWaitGroup{
+        Size: size,
+        
+        current: make(chan struct{}, size),
+        wg:      &sync.WaitGroup{},
+    }
 }
 
 // Add increments the internal WaitGroup counter.
@@ -48,7 +48,7 @@ func New(limit int) SizedWaitGroup {
 //
 // See sync.WaitGroup documentation for more information.
 func (s *SizedWaitGroup) Add() {
-	s.AddWithContext(context.Background())
+    s.AddWithContext(context.Background())
 }
 
 // AddWithContext increments the internal WaitGroup counter.
@@ -60,25 +60,25 @@ func (s *SizedWaitGroup) Add() {
 //
 // See sync.WaitGroup documentation for more information.
 func (s *SizedWaitGroup) AddWithContext(ctx context.Context) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case s.current <- struct{}{}:
-		break
-	}
-	s.wg.Add(1)
-	return nil
+    select {
+    case <-ctx.Done():
+        return ctx.Err()
+    case s.current <- struct{}{}:
+        break
+    }
+    s.wg.Add(1)
+    return nil
 }
 
 // Done decrements the SizedWaitGroup counter.
 // See sync.WaitGroup documentation for more information.
 func (s *SizedWaitGroup) Done() {
-	<-s.current
-	s.wg.Done()
+    <-s.current
+    s.wg.Done()
 }
 
 // Wait blocks until the SizedWaitGroup counter is zero.
 // See sync.WaitGroup documentation for more information.
 func (s *SizedWaitGroup) Wait() {
-	s.wg.Wait()
+    s.wg.Wait()
 }
